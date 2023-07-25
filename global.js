7bd152a35f62d7aebebab8de0cfe412933e3d945
Fleter <69610510+fleters@users.noreply.github.com>
2023-07-25 22:47:13 +0700

    .

---
+++ global.js
@@ -0,0 +1,16 @@
+// Variable
+const website = 'https://fleter.netlify.app';
+const pageSocial = `${website}/html/social/`;
+const pageVINAProject = `${website}/vina-pageVINAProject`;
+
+function defaultPage() {
+  window.location.href = website;
+}
+
+function social() {
+  window.location.href = pageSocial;
+}
+
+function vinaProject() {
+  window.location.href = pageVINAProject;
+}
