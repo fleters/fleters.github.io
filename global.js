84c8e0abd1f5f90a115b5352f3ed8154429c52fb
Fleter <69610510+fleters@users.noreply.github.com>
2023-07-26 15:25:42 +0700

    .

--- global.js
+++ global.js
@@ -1,16 +1,32 @@
-// Variable
+// Main Website
 const website = 'https://fleter.netlify.app';
-const pageSocial = `${website}/html/social/`;
-const pageVINAProject = `${website}/vina-pageVINAProject`;
+
+// Pages of website
+const pSocial = `${website}/html/social/`;
+const VINAProject = `${website}/vina-project/`;
+const fleterServer = `${website}/fleter-server/`;
+const fleterServerAppeal = 'https://docs.google.com/forms/d/e/1FAIpQLSesLvV_mjGURT83jy-B7wzbwWRyneOF0RW5-wCQvlkn1dtWtw/viewform?usp=sf_link';
 
 function defaultPage() {
   window.location.href = website;
 }
 
 function social() {
-  window.location.href = pageSocial;
+  window.location.href = pSocial;
 }
 
 function vinaProject() {
-  window.location.href = pageVINAProject;
+  window.location.href = VINAProject;
+}
+
+function fleterServer(page) {
+  if (page != null) {
+    if (page = 'appeal') {
+      window.location.href = fleterServerAppeal;
+    } else {
+      window.location.href = fleterServer;
+    }
+  } else {
+    window.location.href = fleterServer;
+  }
 }
